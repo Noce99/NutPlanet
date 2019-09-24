b9ffe9be6e74f68ce9f078931d637255a4acb49a
Enrico Mannocci <enrico@PC192.168.1.40.homenet.telecomitalia.it>
2019-09-24 10:38:44 +0200

    Pulizie di Primavera

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo \n\nCompilo!\n\n
-/usr/bin/g++ Launcher.C -O1 -fPIC -std=c++1z -pthread -I /usr/local/include -I /usr/include/GL NutPlanet.h NutPlanet.C -L /usr/local/lib64 -lGG  -lXt -lX11 -lXext -lGL -lGLU -o mio `pkg-config --cflags --libs gtk+-3.0` -L/home/enrico/Programmi/glfw-3.3/glfw-build/src/ -lglfw3 -lGL -lglut -lm -lGLU -Wall -pedantic
